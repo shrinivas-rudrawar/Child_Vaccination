0649744317612b86d0e94b343f893df563c42a29
shrinivas rudrawar <shri78rudrawar80@gmail.com>
2022-02-17 00:08:19 +0530

    Added Exception handler

--- childVaccination_backend/src/main/java/com/app/exception_handler/GlobalExceptionHandler.java
+++ childVaccination_backend/src/main/java/com/app/exception_handler/GlobalExceptionHandler.java
@@ -5,14 +5,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.app.custom_exception.ResourceNotFoundException;
 import com.app.dto.ErrorResponse;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
-
+	
+	//validation exception handler
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
@@ -21,6 +24,16 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("validation error",sb.toString()));
 	}
+	//custom exception handler
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException err){
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("User handling error",err.getMessage()));
+	}
 	
+	//catch all
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<?> handleRuntimeException(RuntimeException err){
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("server side error",err.getMessage()));
+	}
 
 }
