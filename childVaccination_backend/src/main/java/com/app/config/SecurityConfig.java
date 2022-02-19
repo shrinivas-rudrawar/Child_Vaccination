9070eb2282f520f9c3c2bbc8ff8dbd92b468bbce
Rucha Limbekar <ruchalimbekar76@gmail.com>
2022-02-19 19:22:40 +0530

    Applying Usage of UserDetailsService I/F

--- childVaccination_backend/src/main/java/com/app/config/SecurityConfig.java
+++ childVaccination_backend/src/main/java/com/app/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.app.config;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -17,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// TODO Auto-generated method stub
+		auth.userDetailsService(userService);
 		
 	}
 
@@ -27,5 +30,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		
 	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder()
+	{
+		return new BCryptPasswordEncoder();
+	}
 	
 }
