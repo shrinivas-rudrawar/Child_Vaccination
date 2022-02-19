140b88ebe38e6f3cf37d2a730d770141a7c39628
Rucha Limbekar <ruchalimbekar76@gmail.com>
2022-02-19 19:39:22 +0530

    loadByUsername() method in CustomerUserDetailService completed

--- childVaccination_backend/src/main/java/com/app/dao/ILoginDao.java
+++ childVaccination_backend/src/main/java/com/app/dao/ILoginDao.java
@@ -1,10 +1,15 @@
 package com.app.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.app.pojos.Login;
 
+@Repository
 public interface ILoginDao extends JpaRepository<Login, Integer>{
 	Login findByUsernameAndPassword(String username,String passwiord);
 	
+	Optional<Login> findByUsername(String username) ;
 }
--- childVaccination_backend/src/main/java/com/app/service/CustomerUserDetailService.java
+++ childVaccination_backend/src/main/java/com/app/service/CustomerUserDetailService.java
@@ -1,5 +1,32 @@
 package com.app.service;
 
-public class CustomerUserDetailService {
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.app.custom_exception.ResourceNotFoundException;
+import com.app.dao.ILoginDao;
+import com.app.pojos.Login;
+
+
+@Service
+@Transactional
+public class CustomerUserDetailService implements UserDetailsService{
+
+	@Autowired
+	private ILoginDao loginDao;
+	
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		Login login=loginDao.findByUsername(username).orElseThrow(() -> new ResourceNotFoundException("User Name not found !"));
+		
+		return null;
+	}
 
 }
