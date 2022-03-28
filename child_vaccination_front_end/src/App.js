651076784f2ad609096056fbf8b70426953f389e
shrinivas rudrawar <shri78rudrawar80@gmail.com>
2022-03-28 10:25:03 +0530

    react front end startted

---
+++ child_vaccination_front_end/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import MainMenu from './components/MainMenu'
+function App() {
+  return (
+    <div className="App">
+     <MainMenu/>
+    </div>
+  );
+}
+
+export default App;
---
+++ child_vaccination_front_end/src/components/MainMenu.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
+import { Route } from 'react-router';
+import Home from './Home';
+
+
+
+class MainMenu extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            flag: false
+        }
+
+    }
+
+    render(){
+        mystore.subscribe(()=> {this.setState({flag:mystore.getState().loggedin})})
+        return(
+
+        <div className="container">
+            <BrowserRouter>
+            <div className="main"style={{display:this.state.flag?'none':'block'}}>
+                    <ul className="nav" style={{marginLeft:"20%"}}>
+                       <li className="nav-items"><Link className="nav-link" to="/"><b className="b">Home</b> </Link> </li>
+                       <li className="nav-items"><Link  className="nav-link" to="/login"><b className="b">SignIn</b></Link></li>
+                       <li className="nav-items"> <Link  className="nav-link" to="/parentregister"><b className="b">Parent Register</b></Link></li>
+                       <li className="nav-items"> <Link  className="nav-link" to="/hospitalregister"><b className="b">Hospital Register</b></Link></li>
+
+                    </ul>
+                    <div>
+                        <Route  exact path="/" component={Home} /> 
+                    </div>
+
+                </div>
+
+            </BrowserRouter>
+        </div>
+        )
+    }
+
+}
+
