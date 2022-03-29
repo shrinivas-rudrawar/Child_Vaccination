efd557dea06af5d41834f7f6e48cc1057dbbe9bc
shrinivas rudrawar <shri78rudrawar80@gmail.com>
2022-03-29 08:23:03 +0530

    Added back button

--- child_vaccination_front_end/src/components/UpdateHospital.js
+++ child_vaccination_front_end/src/components/UpdateHospital.js
@@ -37,23 +37,26 @@ class UpdateHospital extends React.Component {
                 this.props.history.push("/edithospital");
             })
     }
+    backtoHome  = (e) => {
+        this.props.history.push("/hospitalhome");
+    }
 
 
     render() {
 
         return (
             <div>
+                 <button class="btn btn-primary col-md-1" type="button" onClick={this.backtoHome} style={{ float: "right" }} >Back</button><br/>
                 <div className="container"><br />
-                    <h1 className="header2">Parent Updation</h1>
+                    <h1 className="header2">Hospital Updation</h1>
 
                     <form>
-                        <Form.Group className="mb-3" controlId="formBasicempid">
+                        {/* <Form.Group className="mb-3" controlId="formBasicempid">
                             <Form.Label>EmailID :</Form.Label>
                             <Form.Control type="text" placeholder="Enter empid"
                                 value={this.state.hospital.email}
                             />
-
-                        </Form.Group>
+                        </Form.Group> */}
 
 
                         {/* <div>
@@ -67,23 +70,23 @@ class UpdateHospital extends React.Component {
                         </div> */}
 
 
-                        <div className="form-group" >
+                        <div className="form-group" class="col-md-3">
                             <label className="lb" for="email"> EmailID :</label>
                             <input type="text" id="email" className="form-control" value={this.state.hospital.email} name="email" onChange={this.handleChange} />
                         </div>
-                        <div className="form-group" >
+                        <div className="form-group" class="col-md-3">
                             <label className="lb" for="contactNo">Enter Contact No. :</label>
                             <input type="number" id="contactNo" className="form-control" value={this.state.hospital.contactNo} name="contactNo" onChange={this.handleChange} />
                         </div>
 
 
-                        <div className="form-group" >
+                        <div className="form-group" class="col-md-3">
                             <label className="lb" for="address">Enter Address :</label>
                             <input type="text" id="address" className="form-control" value={this.state.hospital.address} name="address" onChange={this.handleChange} />
                         </div>
 
                         <br></br>
-                        <div className="form-group" class="rows">
+                        <div className="form-group" class="col-md-3">
                             <button type="button" onClick={this.updateData} class="btn btn-primary col-md-5" style={{ marginLeft: "25px" }}>Update</button>
                         </div>
                     </form>
