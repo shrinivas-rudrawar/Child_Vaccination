fb67731898c63cc836286216863587a3b4b8e674
shrinivas rudrawar <shri78rudrawar80@gmail.com>
2022-03-29 09:56:30 +0530

    Added Parent history component

---
+++ child_vaccination_front_end/src/components/GetparentHistory.js
@@ -0,0 +1,69 @@
+
+import React from 'react';
+import axios from 'axios';
+import './mainmenu.css';
+
+class GetparentHistory extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            parentData :[]
+        }
+    }
+    componentDidMount = () => {
+        axios
+            .get(`http://localhost:9090/admin/getparenthistory`)
+            .then(res => {
+                //alert(res);
+                this.setState({ parentData: res.data });
+            });
+    }
+
+    render(){
+        return(
+            <div>
+                <div><br /><br />
+                    <h3 style={{ color: 'wheat' }}>Parent History</h3>
+
+
+                    <table className="table table-bordered table-striped table-dark" style={{ width: "70%" }} responsive="xl">
+                        <thead>
+                            <th>Name</th>
+                            <th>Last Name</th>
+                            <th>Aadhar No</th>
+                            <th>New Email</th>
+                            <th>Old Email</th>
+                            <th>New Address</th>
+                            <th>Old Address</th>
+                            <th>New Contactno</th>
+                            <th>Old Contactno</th>
+                            
+                            <th>Action</th>
+
+                        </thead>
+                        
+                        {this.state.parentData.map(p =>
+
+                            <tbody>
+                                <td>{p.newfname}</td>
+                                <td>{p.newlname}</td>
+                                <td>{p.newadhar_no}</td>
+                                <td>{p.newemail}</td>
+                                <td>{p.oldemail}</td>
+                                <td>{p.newaddress}</td>
+                                <td>{p.oldaddress}</td>
+                                <td>{p.newcontactno}</td>
+                                <td>{p.oldcontactno}</td>                              
+                                <td>{p.action}</td>                               
+                            </tbody>)
+                        }
+                       
+                    </table>
+                </div>
+            </div>
+        )
+    }
+
+
+}
+export default GetparentHistory;
