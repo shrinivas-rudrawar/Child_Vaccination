bce420100fa37daddb7dc747e5f84bbf610d246e
shrinivas rudrawar <shri78rudrawar80@gmail.com>
2022-03-31 15:01:41 +0530

    modified

--- childVaccination_backend/src/main/java/com/app/pojos/Vaccine_Details.java
+++ childVaccination_backend/src/main/java/com/app/pojos/Vaccine_Details.java
@@ -8,6 +8,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
@@ -44,5 +46,19 @@ public class Vaccine_Details implements Serializable{
 	@Column
 	private double price;
 	
+//	@ManyToOne
+//	@JoinColumn(name="hid",nullable = false,updatable = false)
+//	private Hospital hospital;
+//	
+	
+	public Vaccine_Details(String vname,String description,String brand, String type, double price) {		 
+		super();
+		this.vname = vname;
+		this.description = description;
+		this.brand = brand;
+		this.type = type;
+		this.price = price;
+	}
+		
 
 }
