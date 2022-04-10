ffa4efed9f9654af921ffe2cc9629f811aea9fe5
shrinivas rudrawar <shri78rudrawar80@gmail.com>
2022-04-10 13:26:42 +0530

    added passwordEncoder object

--- childVaccination_backend/src/main/java/com/app/dao/IParentDao.java
+++ childVaccination_backend/src/main/java/com/app/dao/IParentDao.java
@@ -4,12 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 
 import com.app.pojos.Parent;
 @Repository
 public interface IParentDao extends JpaRepository<Parent, Integer>{
-	
+
+	PasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
 	@Query(value = "CALL FIND_PARENT_bY_LOGINID(:loginId );", nativeQuery = true)
 	Parent findParent(@Param("loginId") Integer loginId);
 	
