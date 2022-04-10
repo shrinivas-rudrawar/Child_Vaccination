be64fd441973045d4f601f808dadfb3db342a9f3
shrinivas rudrawar <shri78rudrawar80@gmail.com>
2022-04-10 13:29:12 +0530

    modified the login method match with encoded password

--- childVaccination_backend/src/main/java/com/app/service/AdminService.java
+++ childVaccination_backend/src/main/java/com/app/service/AdminService.java
@@ -3,6 +3,7 @@ package com.app.service;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.app.custom_exception.ResourceNotFoundException;
@@ -21,8 +22,9 @@ public class AdminService {
 	
 	public void changePassword(ChangePassword obj) {
 		if(obj.getCnfPassword().equals(obj.getNewPassword())) {
+			String encodedPassword=	new BCryptPasswordEncoder().encode(obj.getCnfPassword());
 			Login login=loginDao.findByUsername(obj.getUsername());
-			login.setPassword(obj.getNewPassword());
+			login.setPassword(encodedPassword);
 		}
 		else
 			throw new ResourceNotFoundException("no user registered with   "+obj.getUsername()+"   username");
